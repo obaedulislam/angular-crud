5ddb7e6625ab7c579a76ba34832f506426dcf379
obaed <obaedul@redlimesolutions.com>
2024-01-18 11:18:45 +0600

    Get data from API function integrated & interface created

---
+++ src/app/service/user.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../interface/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`https://jsonplaceholder.typicode.com/users`);
+  }
+  getUser(): Observable<User> {
+    return this.http.get<User>(`https://jsonplaceholder.typicode.com/user/1`);
+  }
+}
